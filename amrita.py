56dd5d279ea8ab7229fca85c72e7dc66531bfadf
Amrita Singh <56605853+amrita1295@users.noreply.github.com>
2021-06-19 11:54:17 +0530

    Update amrita.py

--- amrita.py
+++ amrita.py
@@ -1,5 +1,20 @@
 import pandas as pd
+import os
+ 
+csv_file = (r"F:\SOB\mempool.csv")
+txt_file = (r"F:\SOB\block.txt")
 
-read_file = pd.read_csv (r'F:\SOB\mempool.csv',delimiter=',')
-read_file.to_csv (r'F:\SOB\block.txt', index=None)
-print("successfully created")
+text_list = []
+
+with open(csv_file, "r") as my_input_file:
+    for line in my_input_file:
+        line = line.split(",")
+        text_list.append(",".join(line))
+
+with open(txt_file, "w") as my_output_file:
+    #my_output_file.write("#1\n")
+   # my_output_file.write("double({},{})\n".format(len(text_list),2))
+  
+    for line in text_list:
+        my_output_file.write("  " + line)
+    print('File Successfully written.')
